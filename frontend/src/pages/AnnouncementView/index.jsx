867d11e3538eb0874a494f6341162183107fe3a8
Eduardo <eduardomgarcia335@gmail.com>
2022-03-29 17:09:11 -0300

    fixed announcement infos

--- frontend/src/pages/AnnouncementView/index.jsx
+++ frontend/src/pages/AnnouncementView/index.jsx
@@ -45,8 +45,6 @@ export const AnnouncementView = () => {
     return lower.charAt(0).toUpperCase() + lower.slice(1);
   }
 
-  const lorem = 'sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
-
   return (
     announcement && (
       <Container>
@@ -70,11 +68,11 @@ export const AnnouncementView = () => {
             )}
             <div className="description">
               <h2>{capitalize(announcement.title)}</h2>
-              <p>{capitalize(lorem)}</p>
+              <p>{capitalize(announcement.description)}</p>
             </div>
             <div className="discipline">
               <h2>{capitalize(announcement.discipline.name)}</h2>
-              <p>{capitalize(lorem)}</p>
+              <p>{capitalize(announcement.discipline.description)}</p>
             </div>
             <div className="calendar">
               <h2 className="labelCalendar">Horários Disponíveis</h2>
